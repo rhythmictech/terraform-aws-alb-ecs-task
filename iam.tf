d092572467aa7e2236406e78338ac5f1c8e8ed17
Steven Black <genseb7@gmail.com>
2020-06-10 17:50:10 -0400

    use aws role

--- iam.tf
+++ iam.tf
@@ -1,10 +1,9 @@
 ########################################
 # I is for IAM, and this module creates two roles
 #
-# ECS Task Execution Role
-#   IAM role used by the task itself
-#    e.g. if your container wants to call other AWS services like S3, SQS, etc
-#    then those permissions would need to be covered by the TaskRole
+# ECS Task Execution Role, IAM role used by the task itself
+#   e.g. if your container wants to call other AWS services like S3, SQS, etc
+#   then those permissions would need to be covered by the TaskRole
 ########################################
 data "aws_iam_policy_document" "assume" {
   statement {
@@ -33,26 +32,26 @@ resource "aws_iam_role_policy_attachment" "additional" {
 ########################################
 # ECS Execution Role
 #   IAM role that executes ECS actions such as pulling the image and storing the application logs in cloudwatch
+#   https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task_execution_IAM_role.html
 ########################################
 resource "aws_iam_role" "ecs_exec" {
   name_prefix        = local.ecs_exec_iam_role_name_prefix
-  assume_role_policy = data.aws_iam_policy_document.assume.json
+  assume_role_policy = data.aws_iam_policy_document.ecs_exec.json
   tags               = var.tags
 }
 
 data "aws_iam_policy_document" "ecs_exec" {
   statement {
-    effect    = "Allow"
-    resources = ["*"]
-    actions = [
-      "logs:CreateLogStream",
-      "logs:PutLogEvents"
-    ]
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
   }
 }
 
-resource "aws_iam_role_policy" "ecs_exec" {
-  name_prefix = "${var.name}-ecs-exec-"
-  policy      = data.aws_iam_policy_document.ecs_exec.json
-  role        = aws_iam_role.ecs_exec.id
+resource "aws_iam_role_policy_attachment" "ecs_exec" {
+  role       = aws_iam_role.ecs_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
