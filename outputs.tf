02e51c677ad08e99eafeed9ebcbd470a1360b231
Steven Black <genseb7@gmail.com>
2021-01-19 13:50:19 -0500

    alphabetize outputs

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,10 @@
 ########################################
 # outputs
 ########################################
+
 output "cloudwatch_log_group" {
   description = "aws_cloudwatch_log_group resource"
   value       = aws_cloudwatch_log_group.this
-}
-
-output "lb_target_group" {
-  description = "aws_lb_target_group resource"
-  value       = aws_lb_target_group.this
 }
 
 output "ecs_service" {
@@ -16,14 +12,19 @@ output "ecs_service" {
   value       = aws_ecs_service.this
 }
 
+output "ecs_task_iam_role" {
+  description = "aws_iam_role resource for the ECS task"
+  value       = aws_iam_role.ecs_task
+}
+
 output "iam_role_ecs_service" {
   description = "aws_iam_role resource for the ECS service"
   value       = aws_iam_role.ecs_exec
 }
 
-output "ecs_task_iam_role" {
-  description = "aws_iam_role resource for the ECS task"
-  value       = aws_iam_role.ecs_task
+output "lb_target_group" {
+  description = "aws_lb_target_group resource"
+  value       = aws_lb_target_group.this
 }
 
 output "task_definition" {
